2945cef7139be9a2b264d0537a6d12b92700cabf
paulo-pssj <paulinhops99@gmail.com>
2022-02-16 16:41:56 -0300

    First commit

---
+++ airflow/plugins/airflow_plugin.py
@@ -0,0 +1,8 @@
+from airflow.plugins_manager import AirflowPlugin
+from operators.twitter_operator import TwitterOperator
+
+
+class AluraAirflowPlugin(AirflowPlugin):
+    name = "alura"
+    operators = [TwitterOperator]
+    
